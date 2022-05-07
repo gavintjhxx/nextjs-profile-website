7c7d3dca430538343d8daa29f59dbed4917d8986
gavintjhxx <gavintjhx@gmail.com>
2022-05-07 23:02:59 +0800

    [FIX] Function name

--- pages/commissions.js
+++ pages/commissions.js
@@ -1,17 +1,15 @@
 import BlogNavbar from "../components/blog/blogNavBar"
 import BlogHeader from "../components/blog/blogHeader"
-import BlogAuthor from "../components/blog/blogAuthor"
 import BlogDivider from "../components/blog/blogDivider"
 import BlogSectionText from "../components/blog/blogSectionText"
 import BlogSectionHeader from "../components/blog/blogSectionHeader"
 import BlogSectionEmbed from "../components/blog/blogSectionEmbed"
 import BlogFooter from "../components/blog/blogFooter"
 import BlogSectionEmbedList from "../components/blog/blogSectionEmbedList"
-import BlogImage from "../components/blog/blogImage"
 import BlogLink from "../components/blog/blogLink"
 import ToS from "../components/commissions/tos"
 
-export default function About() {
+export default function Commissions() {
     return (
         <>
             <head>

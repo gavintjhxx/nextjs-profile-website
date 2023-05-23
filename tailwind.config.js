c308fb746f23773c8fcc7149102225330b4072ce
unknown <gavintjhx@gmail.com>
2023-05-23 22:27:48 +0800

    [CHANGE] updated website colour palette

--- tailwind.config.js
+++ tailwind.config.js
@@ -13,23 +13,30 @@ module.exports = {
     },
     theme: {
         colors: {
-            background: colors.coolGray[50],
-            titles: colors.blueGray[800],
+            background: colors.coolGray[100],
+            titles: colors.coolGray[800],
             texts: colors.coolGray[600],
-            links: colors.blueGray[400],
-            linksHover: colors.blue[400],
+            links: colors.blueGray[500],
+            linksHover: colors.blue[700],
             blogWidget: colors.coolGray[100],
             blogWidgetTag: colors.blueGray[600],
             blogWidgetTagDesc: colors.blueGray[100],
             blogWidgetTitle: colors.blueGray[800],
             blogs: {
                 titles: colors.coolGray[800],
-                texts: colors.coolGray[700],
-                links: colors.coolGray[500],
-                linksHover: colors.coolGray[700],
-                navBackgrounds: colors.coolGray[200],
+                texts: colors.coolGray[600],
+                links: colors.blueGray[500],
+                linksHover: colors.blue[700],
                 borders: colors.coolGray[800]
+            },
+            navbars: {
+                background: colors.blueGray[800],
+                titles: colors.coolGray[200],
+                texts: colors.coolGray[400],
+                links: colors.blueGray[400],
+                linksHover: colors.blueGray[600]
             }
+            // TODO: Add dark mode colour palette
         }
     }
     // ...

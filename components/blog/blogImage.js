562b14c62e646df13589c8565ec10767a9861f88
unknown <gavintjhx@gmail.com>
2023-05-23 21:51:14 +0800

    [change] styling max height

--- components/blog/blogImage.js
+++ components/blog/blogImage.js
@@ -1,5 +1,5 @@
 export default function BlogImage(data) {
     return (
-        <img className="w-10/12 max-h-[32rem] object-scale-down bg-background rounded-lg m-8 shadow-xl" src={data.src} alt={data.alt} data-aos="zoom-in" />
+        <img className="w-10/12 max-h-[32rem] object-scale-down bg-background rounded-lg my-8 shadow-xl mx-auto" src={data.src} alt={data.alt} data-aos="zoom-in" />
     )
 }
